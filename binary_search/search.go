89f10d6d45d2cf22f51e8d0ff4a4e082a9a7a211
leave8080 <wunaiwei8080@gmail.com>
2023-08-16 14:01:31 +0800

    二分查找

--- binary_search/search.go
+++ binary_search/search.go
@@ -25,3 +25,168 @@ func binarySearch(nums []int, target int, findFirst bool) int {
 	}
 	return left
 }
+
+/**********************************************************************************/
+func searchFloor(arr []int, tar int) int {
+	l, r := 0, len(arr)-1
+	for l <= r {
+		mid := l + (r-l)/2
+		if arr[mid] >= tar {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	if l < len(arr) && arr[l] == tar {
+		return l
+	}
+	return -1
+}
+
+func searchCeil(arr []int, tar int) int {
+	l, r := 0, len(arr)-1
+	for l <= r {
+		mid := l + (r-l)/2
+		if arr[mid] <= tar {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	if r >= 0 && arr[r] == tar {
+		return r
+	}
+	return -1
+}
+
+/**********************************************************************************/
+/*
+704 左闭右闭
+*/
+func searchRange2(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		middle := left + (right-left)>>1
+		if target < nums[middle] {
+			right = middle - 1
+		} else if target > nums[middle] {
+			left = middle + 1
+		} else {
+			return middle
+		}
+	}
+	return -1
+}
+
+/*
+左闭右开
+*/
+func searchRange3(nums []int, target int) int {
+	left, right := 0, len(nums)
+	for left < right {
+		middle := left + (right-left)>>1
+		if target < nums[middle] {
+			right = middle
+		} else if target > nums[middle] {
+			left = middle + 1
+		} else {
+			return middle
+		}
+	}
+	return -1
+}
+
+/**********************************************************************************/
+
+/*
+34
+给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+如果数组中不存在目标值 target，返回 [-1, -1]。
+你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+*/
+/*
+方法1
+*/
+func searchRange4(nums []int, target int) []int {
+	left := leftRange(nums, target)
+	if left == len(nums) || nums[left] != target {
+		return []int{-1, -1}
+	}
+	right := leftRange(nums, target+1) - 1
+	return []int{left, right}
+}
+
+func leftRange(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		middle := left + (right-left)>>1
+		if target < nums[middle] {
+			right = middle - 1
+		} else if target > nums[middle] {
+			left = middle + 1
+		}
+	}
+	return left
+}
+
+/*
+方法2
+*/
+func leftRangeFunc(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	start := -1
+	for left <= right {
+		middle := left + (right-left)>>1
+		if target <= nums[middle] {
+			right = middle - 1
+		} else {
+			left = middle + 1
+		}
+		if nums[middle] == target {
+			start = middle
+		}
+	}
+	return start
+}
+func rightRangeFunc(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	end := -1
+	for left <= right {
+		middle := left + (right-left)>>1
+		if target >= nums[middle] {
+			left = middle + 1
+		} else {
+			right = middle - 1
+		}
+		if nums[middle] == target {
+			end = middle
+		}
+	}
+	return end
+}
+func searchRange5(nums []int, target int) []int {
+	return []int{leftRangeFunc(nums, target), rightRangeFunc(nums, target)}
+}
+
+/**********************************************************************************/
+
+/*
+35
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+
+请必须使用时间复杂度为 O(log n) 的算法。
+*/
+func searchInsert(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		middle := left + (right-left)>>1
+		if target < nums[middle] {
+			right = middle - 1
+		} else if target > nums[middle] {
+			left = middle + 1
+		} else {
+			return middle
+		}
+	}
+	return left
+}
--- binary_search/search_test.go
+++ binary_search/search_test.go
@@ -10,3 +10,20 @@ func Test_searchRange(t *testing.T) {
 	target := 5
 	fmt.Println(searchRange(nums, target))
 }
+
+func Test_searchCeil(t *testing.T) {
+
+	fmt.Println(searchCeil([]int{1, 2, 3, 3, 3, 5, 5, 5, 6}, 3))
+	fmt.Println(searchFloor([]int{1, 2, 3, 3, 3, 5, 5, 5, 6}, 3))
+
+	left := 1
+	right := 3
+	fmt.Println(left + (right-left)>>1)
+}
+
+func Test_searchRange2(t *testing.T) {
+
+	//fmt.Println(searchRange2([]int{1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 4, 5, 6, 7, 8, 9}, 3))
+	//fmt.Println(searchRange3([]int{2, 5}, 5))
+	fmt.Println(searchRange5([]int{1, 2, 2, 3, 3, 3, 4, 5, 6, 7, 8, 9}, 3))
+}
